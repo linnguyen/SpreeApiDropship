18574b23a4fee28788117588046b485d542e8dd0
Ryne <ryne@enclave.vn>
2018-12-21 10:39:14 +0700

    Intialize

---
+++ app/src/main/java/com/example/ryne/myapplication/Kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ryne.myapplication.Kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ryne.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
